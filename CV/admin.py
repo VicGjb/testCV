e055ff254e6048e4aa27581d7f2bf129dcef494f
Admin Ad <avigdorbrandt@gmail.com>
2021-11-21 16:41:34 +0200

    fix settings and models. Added admin models

--- CV/admin.py
+++ CV/admin.py
@@ -1,3 +1,168 @@
+from os import read
+from ckeditor import widgets
 from django.contrib import admin
+from django import forms
+from ckeditor.widgets import CKEditorWidget
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django.db.models import fields
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Person, WorkHistory, Skill, Project, PersonSkill
+
+admin.site.site_header = "CV"
+admin.site.site_title = "CV"
+
+"""**********************************FORMS**************************************"""
+class PersonAdminForm(forms.ModelForm):
+    """add CKeditor in person form"""
+    about = forms.CharField(label='Professional summary', widget=CKEditorWidget())
+    class Meta:
+        model = Person
+        fields = '__all__'
+
+
+class WorkHistoryAdminForm(forms.ModelForm):
+    """add CKeditor in WorkHistory form"""
+    description = forms.CharField(label='About your work', widget=CKEditorWidget())
+
+    class Meta:
+        model = WorkHistory
+        fields = '__all__'
+
+
+class ProjectAdminForm(forms.ModelForm):
+    """add CKeditor in Project form"""
+    description = forms.CharField(label='About your work', widget=CKEditorWidget())
+
+    class Meta:
+        model = WorkHistory
+        fields = '__all__'
+
+"""**********************************InLines**************************************"""
+class WorkHistoryInLine(admin.TabularInline):
+    """Inline WorkHistory in PesonAdmin"""
+    model = WorkHistory
+    extra = 1
+    fieldsets = (
+        (None, {
+            "fields": (
+                (('worker', 'company', 'position', 'date_start', 'date_finish'),)
+            ),
+        }),
+        (None, {
+            "fields": (
+                (('description',),)
+            ),
+        }),
+    )
+
+class ProjectInLine(admin.TabularInline):
+    """Inline Projects in PesonAdmin"""
+    model = Project
+    extra = 1
+    fields = ('name','link','description')
+    
+class SkilsInLine(admin.StackedInline):
+    """Inline Skils in PesonAdmin"""
+    model = PersonSkill
+    extra = 3
+
+
+"""**********************************Admins**************************************"""
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    """Person"""
+    list_display = ('id','name','surname', 'get_image')
+    list_display_links = ('id','name','surname')
+    search_fields = ('name','urname',)
+    save_on_top=True
+    form=PersonAdminForm
+    inlines = [SkilsInLine, WorkHistoryInLine, ProjectInLine]
+    readonly_fields = ('get_image',)
+    fieldsets = (
+        (None, {
+            "fields": (
+                (('name', 'surname','photo', 'get_image',),)
+            ),
+        }),
+        (None, {
+            "fields": (
+                (('phone', 'email','city'),)
+            ),
+        }),
+                (None, {
+            "fields": (
+                ('about',)
+            ),
+        }),
+    )
+    
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.photo.url} width="110" height="110"')
+    get_image.short_description='Photo'
+
+
+@admin.register(WorkHistory)
+class WorkHistoryAdmin(admin.ModelAdmin):
+    """work History"""
+    list_display = ('worker', 'company', 'position', 'date_start', 'date_finish')
+    list_display_links = ('worker', 'company', 'position')
+    search_fields = ('person',)
+    save_on_top = True
+    save_as = True
+    form = WorkHistoryAdminForm
+    fieldsets = (
+        (None, {
+            "fields": (
+                (('worker'),)
+            ),
+        }),
+        (None, {
+            "fields": (
+                (('company','position'),)
+            ),
+        }),     
+        (None, {
+            "fields": (
+                (('date_start','date_finish'),)
+            ),
+        }),
+        (None, {
+            "fields": (
+                (('description'),)
+            ),
+        }),   
+    )
+    
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    """Skills"""
+    list_display = ('name',)
+    list_display_links = ('name',)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    """Projects"""
+    list_display = ('person', 'name', 'link')
+    list_display_links = ('person', 'name') 
+    search_fields = ('person',)
+    form = ProjectAdminForm
+    fieldsets = (
+        (None, {
+            "fields": (
+                (('person'),)
+            ),
+        }),
+        (None, {
+            "fields": (
+                (('name', 'link'),)
+            ),
+        }),
+                (None, {
+            "fields": (
+                (('description'),)
+            ),
+        }),
+    )
--- CV/models.py
+++ CV/models.py
@@ -1,32 +1,50 @@
+from django.core import validators
 from django.db import models
 from django.db.models.aggregates import Max
 from django.db.models.deletion import CASCADE
 from django.db.models.fields.related import ManyToManyField
+from django.core.validators import RegexValidator
 
 
 class Person(models.Model):
     """Person"""
     name = models.CharField(max_length=20, verbose_name='name')
-    surname = models.CharField(max_length=30, verbose_name='name')
-    photo = models.ImageField()
-    about  = models.TextField(max_length=500, verbose_name='professional summary')
-
+    surname = models.CharField(max_length=30, verbose_name='surname')
+    photo = models.ImageField('Avatar', upload_to="Person's photo")
+    about  = models.TextField(max_length=500, verbose_name='Professional summary')
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+        )
+    phone = models.CharField(
+        validators=[phone_regex], max_length=17, verbose_name='phone', blank=True, null=True
+        )
+    email = models.CharField(max_length=40, verbose_name='e-mail', blank=True, null = True)
+    city =  models.CharField(max_length=100, verbose_name='address', blank=True, null = True)
     class Meta:
         verbose_name='Person'
         verbose_name_plural='Persons'
+    
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+
 
 class WorkHistory(models.Model):
     """Work experience"""
-    worker = models.ForeignKey(Person, on_delete=CASCADE, primary_key=True, related_name='work_history')
+    worker = models.ForeignKey(Person, on_delete=CASCADE, related_name='work_history')
     company = models.CharField(max_length=50, verbose_name='company name')
     position = models.CharField(max_length=20, verbose_name='position')
     date_start = models.DateField(verbose_name='start date')
-    date_finish = models.DateField(verbose_name='finish date', null=True)
-    description = models.TextField(verbose_name='about your work', null=True)
+    date_finish = models.DateField(verbose_name='finish date', blank=True, null=True)
+    description = models.TextField(verbose_name='about your work',blank=True, null=True)
 
     class Meta:
         verbose_name='Work'
         verbose_name_plural='Works'
+
+    def __str__(self):
+        return f'{self.company} {self.position}'
+
 
 class Skill(models.Model):
     """Professional Skills"""
@@ -36,14 +54,29 @@ class Skill(models.Model):
     class Meta:
         verbose_name='Skill'
         verbose_name_plural='Skills'
+    
+    def __str__(self):
+        return self.name
+    
+
 
 class Project(models.Model):
     """Portfolio projects"""
     person = models.ForeignKey(Person, on_delete=CASCADE, related_name='project')
     name = models.CharField(max_length=20, verbose_name='project name')
     link = models.CharField(max_length=50, null=True, verbose_name='link to project site')
-    description = models.TextField(max_length=500, null=True, verbose_name='description of your project')
+    description = models.TextField(
+        max_length=500, null=True, verbose_name='description of your project'
+        )
 
     class Meta:
         verbose_name = 'Project'
         verbose_name_plural = 'Projects'
+
+    def __str__(self):
+        return f"{self.person} project: {self.name}"
+    
+
+class PersonSkill(models.Model):
+    person = models.ForeignKey(Person, on_delete=CASCADE)
+    skill = models.ForeignKey(Skill, on_delete=CASCADE)
